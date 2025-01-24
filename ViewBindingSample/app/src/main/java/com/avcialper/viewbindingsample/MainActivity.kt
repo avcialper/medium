2b4d14d626c9b16cadfc0a3b917971e4be8cb748
avcialper <avcialper12@gmail.com>
2025-01-24 20:02:29 +0300

    initial commit with <3

---
+++ ViewBindingSample/app/src/main/java/com/avcialper/viewbindingsample/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.avcialper.viewbindingsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.avcialper.viewbindingsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            btnChange.setOnClickListener {
+                val title = txtTitle.text
+
+                if (title.equals("Hello World!"))
+                    txtTitle.text = "Hello Medium!"
+                else
+                    txtTitle.text = "Hello World!"
+            }
+        }
+    }
+
+    private fun defaultUISettings() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
