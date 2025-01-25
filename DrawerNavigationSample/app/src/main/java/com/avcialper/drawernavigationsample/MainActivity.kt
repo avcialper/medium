ec3deca127ee583bbf44c33d6160f087a0bd7177
avcialper <avcialper12@gmail.com>
2025-01-25 22:55:09 +0300

    Add: NavigationDrawerSample

---
+++ DrawerNavigationSample/app/src/main/java/com/avcialper/drawernavigationsample/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.avcialper.drawernavigationsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.avcialper.drawernavigationsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Burger ikonunu ve geri tuşunu aktifleştir.
+        setupActionBarWithNavController(navController, binding.main)
+
+        // Navigation drawer ile navController'ı birleştirir.
+        binding.navigationDrawer.setupWithNavController(navController)
+
+        defaultUISettings()
+    }
+
+    // Burger ikonuna basıldığında drawer'ı açar. Up buttonu aktif hale getirir.
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.main)
+    }
+
+    private fun defaultUISettings() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
